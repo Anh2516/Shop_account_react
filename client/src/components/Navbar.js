16be41bd97678f2a50d860928ba5b13c7c333c1d
Anh2516 <anhdt.b24cc009@stu.ptit.edu.vn>
2025-12-06 14:09:10 +0700

    Fix: Lưu giỏ hàng vào localStorage để không mất dữ liệu khi refresh trang

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../store/slices/authSlice';
+import { clearCart } from '../store/slices/cartSlice';
 import { formatPrice } from '../utils/formatPrice';
 import './Navbar.css';
 
@@ -13,6 +14,7 @@ const Navbar = () => {
 
   const handleLogout = () => {
     dispatch(logout());
+    dispatch(clearCart()); // Clear cart when logging out
     navigate('/');
   };
 
